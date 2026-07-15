8a3ded2686da64fcb86865fdd310de60fe0e59e0
Parvateesh Gutti <parvateeshgutti@Mac.attlocal.net>
2026-07-15 10:34:25 -0500

    Add Home back to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 const links = [
+  { href: '/', label: 'Home' },
   { href: '/events', label: 'Events' },
   { href: '/menu', label: 'Menu' },
   { href: '/parties', label: 'Parties' },
